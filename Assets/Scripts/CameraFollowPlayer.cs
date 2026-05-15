7eac930831024e9d6cc4dc9ecc92cc54768fdd95
ChronoSariel <86979628+ChronoSariel@users.noreply.github.com>
2026-05-14 23:58:39 -0400

    Level

--- Assets/Scripts/CameraFollowPlayer.cs
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class CameraFollowPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject Player;
+    private float cameraSpeed = 3f;
+    private Vector3 offset = new Vector3(0f, 10f, -10f);
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
@@ -13,6 +16,10 @@ public class CameraFollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, Player.transform.position + offset, cameraSpeed * Time.deltaTime);
     }
 }
